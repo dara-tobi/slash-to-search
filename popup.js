9f70e04d71bba18fb927b1f46d5a77fa2040da86
Dara Oladosu <daradosu@gmail.com>
2019-01-03 21:00:56 +0100

    add popup options for toggling extension and autofocus

---
+++ popup.js
@@ -0,0 +1,80 @@
+(function(){
+
+  var optionEnabled = document.querySelector('#optionEnabled');
+  var optionAutofocus = document.querySelector('#optionAutofocus');
+
+  if (optionAutofocus) {
+    optionAutofocus.addEventListener('change', updateOptions);
+  }
+
+  if (optionEnabled) {
+    optionEnabled.addEventListener('change', updateOptions);
+  }
+
+  function updateOptions () {
+
+    if (optionEnabled && optionEnabled.checked) {
+      var isEnabled = true;
+
+      // slash to search is now enabled
+    } else {
+      // slash to search is disabled
+      if (optionAutofocus) {
+        optionAutofocus.checked = false;
+      }
+    }
+
+
+    if (optionAutofocus && optionAutofocus.checked) {
+      var isAutofocus = true;
+
+      // Autofocus is now enabled
+    } else {
+      // Autofocus is disabled
+    }
+
+    chrome.tabs.query({'active': true, 'currentWindow': true}, function(tab) {
+      var url = tab[0].url;
+      var domain = url.split('//')[1].split('/')[0];
+
+
+      chrome.storage.sync.get(['disabledSites', 'autofocusSites'], function(sites) {
+        var disabledSites = [];
+        var autofocusSites = [];
+
+        if (sites.disabledSites) {
+          disabledSites = sites.disabledSites;
+        }
+
+        if (sites.autofocusSites) {
+          autofocusSites = sites.autofocusSites;
+        }
+
+        if (!isEnabled) {
+          // disabling slash to search for domain
+          disabledSites.push(domain);
+        } else {
+          // enabling slash to search for domain
+          var domainIndex = disabledSites.indexOf(domain);
+          disabledSites.splice(domainIndex, 1);
+        }
+
+        if (isAutofocus && !disabledSites.includes(domain)) {
+          // enabling autofocus for domain
+          autofocusSites.push(domain);
+        } else {
+          // disabling autofocus for domain
+          autofocusSites.splice(domainIndex, 1);
+        }
+
+
+        chrome.storage.sync.set({
+          disabledSites: disabledSites,
+          autofocusSites: autofocusSites
+        }, function() {
+
+        });
+      });
+    });
+  }
+})();
