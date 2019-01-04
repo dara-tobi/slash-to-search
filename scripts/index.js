4775e8c3173b6c3d363bcd30fa9e1d8ffcd3831f
Dara Oladosu <daradosu@gmail.com>
2019-01-04 15:44:25 +0100

    enable extending of extension to account for multiple input types and indexes

--- scripts/index.js
+++ scripts/index.js
@@ -1,45 +1,48 @@
 // Slash to search
 (function() {
 
-
   chrome.runtime.onMessage.addListener(
     function(request, sender, sendResponse) {
+
       if (request.message === 'pageLoaded') {
+        if (pageHasSearchOrTextInputTypes()) {
 
-        chrome.storage.sync.get(['disabledSites', 'autofocusSites'], function(sites) {
-          var disabledSites = sites.disabledSites || [];
-          var autofocusSites = sites.autofocusSites || [];
-          var url = window.location.href;
-          var domain = url.split('//')[1].split('/')[0];
-          
+          chrome.storage.sync.get(['disabledSites', 'autofocusSites'], function(sites) {
 
-          if (disabledSites.includes(domain)) {
-            // slash is disabled for this domain
-            return null;
-          } else {
-            // slash is enabled for this domain
-          }
+            var disabledSites = sites.disabledSites || [];
+            var autofocusSites = sites.autofocusSites || [];
 
-          if (autofocusSites.includes(domain)) {
-            // auto is on for this domain, setting focus
-            setFocus();
-          }
+            var domain = getDomain();
 
-          // add slash key shortcut, regardless of autofocus
-          document.addEventListener('keyup', e => {
-            if (e.key === '/') {
-              // '/' clicked, setting focus
+            if (disabledSites.includes(domain)) {
+              // slash is disabled for this domain
+              return null;
+            } else {
+              // slash is enabled for this domain
+            }
+
+            if (autofocusSites.includes(domain)) {
+              // auto is on for this domain, setting focus
               setFocus();
             }
+
+            // add slash key shortcut, regardless of autofocus
+            document.addEventListener('keyup', e => {
+              if (e.key === '/') {
+                // '/' clicked, setting focus
+                setFocus();
+              }
+            });
           });
-        });
+        }
       }
   });
 
 
   function setFocus() {
 
-    var searchBox = document.querySelector('input[type=text]');
+    var searchBox = getSearchBox();
+
     var searchBoxValue = searchBox.value;
     var searchBoxValueLength = searchBoxValue.length || 0;
 
@@ -49,5 +52,52 @@
     // Ensure the cursor is at the end of the text
     searchBox.setSelectionRange(searchBoxValueLength, searchBoxValueLength);
 
-  }  
+  }
+
+  function pageHasSearchOrTextInputTypes() {
+
+    if (document.querySelector('input[type=text]')
+      || document.querySelector('input[type=search]')) {
+      return true;
+    }
+
+    return false;
+  }
+
+  function getSearchBox() {
+
+    var searchBoxType = getSearchBoxType();
+
+    var boxTypeParts = searchBoxType.split('Input');
+    var typeName = boxTypeParts[0];
+    var typeIndex = boxTypeParts[1];
+
+    return document.querySelectorAll(`input[type=${typeName}]`)[ typeIndex - 1 ];
+
+  }
+
+  function getDomain() {
+
+    var url = window.location.href;
+    return url.split('//')[1].split('/')[0];
+
+  }
+
+  function getSearchBoxType() {
+
+    var domain = getDomain();
+
+    var searchBoxTypes = searchBoxTypesConfig;
+
+    for (boxType in searchBoxTypes) {
+
+      if (searchBoxTypes[boxType].includes(domain)) {
+
+        return boxType;
+      }
+    }
+
+    return 'textInput1';
+
+  }
 })();
---
+++ scripts/searchBoxTypesConfig.js
@@ -0,0 +1,17 @@
+/*
+  For websites where the search box isn't the first text input type,
+  indicate the type (<input type = 'text'> or <input type = 'search'>) of input
+  and the index location (document.querySelectoryAll()[1] or document.querySelectoryAll()[2] or [3] etc)
+  of the input, where the search box can be found.
+
+  Indicate the search box type and index in the format ${type}Input${Index}.
+
+  A single type-index combo can be found on multiple websites. A type-index combo is the object
+  key for an array of all the websites where that combo can be found...
+*/
+var searchBoxTypesConfig = {
+
+  searchInput1: ['medium.com'],
+  textInput2: ['mail.google.com']
+
+};
