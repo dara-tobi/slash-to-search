99b7a6708643a7e4d66dc9bf942c67c3091b77b0
Dara Oladosu <daradosu@gmail.com>
2019-01-27 19:09:54 +0100

    ensure configured element gets returned

--- scripts/index.js
+++ scripts/index.js
@@ -145,8 +145,9 @@
 
   function pageHasSearchOrTextInputTypes() {
 
-    if (document.querySelector('input[type=text]')
-      || document.querySelector('input[type=search]')) {
+    if (
+      document.querySelector('[class*=search i], [id*=search i]')
+    ) {
       return true;
     }
 
@@ -245,7 +246,7 @@
       }
     }
 
-    return searchElement;
+    return searchElement || configuredElement;
 
   }
 
